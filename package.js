d80cdb1627556eac16e58c1e4bd90c7567aaeedb
Evan Tann <egtann@gmail.com>
2012-12-22 13:57:19 -0500

    Bugfix for missing file

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('pagination_client.js', 'client');
+  api.add_files('pagination.js', 'client');
 });
 
 Package.on_test(function (api) {
